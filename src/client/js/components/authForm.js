e65123b548cb0c4d2505983c46a38670d6a8ec86
Exomelech <anterra.dm@gmail.com>
2019-09-14 21:00:01 +0300

    auth mocks

--- src/client/js/components/authForm.js
+++ src/client/js/components/authForm.js
@@ -1,15 +1,11 @@
 import { Link } from 'react-router-dom';
+import data from '../../mocks/auth-form.json';
 
 export default class AuthForm extends React.Component {
   render() {
     const { type } = this.props;
-    const name_pattern = '^[А-Яа-яA-Za-z0-9_-]+$'
-    const pattern = '^[A-Za-z0-9]+$'
     if( type==='registration' || type==='login' ){
-      let form_title = type==='login' ? 'Вход на сайт' : 'Регистрация';
-      let button_title = type==='login' ? 'Войти' : 'Зарегистрироваться';
-      let nav_title = type==='login' ? 'Ещё не зарегестрированы?' : 'Уже зарегестрированы?';
-      let link = type==='login' ? '/unsigned/registration/' : '/unsigned/login/';
+      const { name_pattern, pattern, form_title, button_title, nav_title, link } = data[type];
       return (
         <form className='auth-form' name='login_form'>
           <div className='auth-form__title'>{form_title}</div>
@@ -24,8 +20,8 @@ export default class AuthForm extends React.Component {
           <div className='auth-form__block'>
             <input type='password' className='auth-form__input' id='auth_password' placeholder='Пароль' required pattern={pattern} minLength="6"></input>
           </div>
-          <button className='auth-form__submit' type="submit">{button_title}</button> 
-          <Link to={link} className='auth-form__navButton'>{nav_title}</Link>
+          <button className='auth-form__button-submit' type="submit">{button_title}</button> 
+          <Link to={link} className='auth-form__button-submit'>{nav_title}</Link>
         </form>
       );
     }else{
